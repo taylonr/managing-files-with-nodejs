88d8bbfb2d5af9507aeee7fec41d37058a3db95c
Nate Taylor <taylonr@gmail.com>
2019-09-21 08:36:04 -0500

    Completing the partial reads

---
+++ synchronous.read.partial.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+const fd = fs.openSync("./data/app.log");
+
+let count = 0;
+
+do{
+    const buffer = Buffer.alloc(200);
+    count = fs.readSync(fd, buffer, 0, buffer.length, null);
+    console.log(buffer.toString());
+}while(count > 0);
+
