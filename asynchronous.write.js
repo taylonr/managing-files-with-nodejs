d55bb084b04efeb868a49b2ec5fac38aee7f33ae
Nate Taylor <taylonr@gmail.com>
2021-02-13 09:22:00 -0600

    Finishing streams

--- asynchronous.write.js
+++ asynchronous.write.js
@@ -2,7 +2,7 @@ const {constants, writeFile} = require('fs');
 
 writeFile('./data/newapp.log',
     '163.3.217.18 - - [21/09/2019:10:07:21 -0500] "GET /write-file-test" 405 21512',
-    {encoding: 'base64'},
+    {mode: constants.S_IRUSR | constants.S_IWUSR},
     (err) => {
         err ? console.log(err) : console.log("file saved!");
     });
